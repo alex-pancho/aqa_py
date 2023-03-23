56fe296feccfb3eec35df330c5d51251eadc7590
alexmudra <ko9#ba9$>
2023-03-23 15:07:30 +0200

    task 10 done

--- lession_03/homeworks03.py
+++ lession_03/homeworks03.py
@@ -65,7 +65,7 @@ print("Task 6->>>", second_occurence)
 Збережіть результат у змінній adwentures_of_tom_sawer_sentences
 """
 adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split('\n')
-# print("Task 7->>> \n{}".format (adwentures_of_tom_sawer_sentences))
+print("Task 7->>> \n{}".format (adwentures_of_tom_sawer_sentences))
 
 
 # task 08
@@ -78,8 +78,21 @@ print("Task 8 v2->>>", adwentures_of_tom_sawer_sentences[3].casefold())
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
-
+phrase = "By the time"
+count_numb = 0
+count_numb_sentanse = []
+for i in adwentures_of_tom_sawer_sentences:
+    count_numb +=1
+    if i.startswith(phrase):
+        count_numb_sentanse.append(count_numb)
+        print("The expected-phrase is located in sentanse", count_numb_sentanse)
+        continue
+    else:
+        print("There is no expected phrase in sentanse",  count_numb)
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+last_sentence = adwentures_of_tom_sawer_sentences[-1]
+words_list = last_sentence.split()
+print("The last sentence has {}".format(len(words_list)), "words")
