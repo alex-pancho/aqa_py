e40e2524bb5e4b49f5d9314a7d3267c49f7c901e
Olena <el.oliinyk@gmail.com>
2023-04-14 14:01:40 +0300

    Homework_8

--- lession_06/homeworks06.py
+++ lession_06/homeworks06.py
@@ -8,16 +8,16 @@ def multiplication_table(number):
     multiplier = 1
 
     # Complete the while loop condition.
-    while multiplier <= number:
+    while multiplier <= 10:
         result = number * multiplier
         # десь тут помила, а може не одна
-        if  result > "25":
+        if  result > 25:
             # Enter the action to take if the result is greater than 25
-            pass
+            break
         print(str(number) + "x" + str(multiplier) + "=" + str(result))
 
         # Increment the appropriate variable
-        multi += 1
+        multiplier += 1
 
 multiplication_table(3)
 # Should print:
@@ -31,28 +31,46 @@ multiplication_table(3)
 # task 2
 """  Написати функцію, яка обчислює суму двох чисел.
 """
-
+def sum_num(a, b):
+    return (a + b)
+sum_num(4,5)
 
 # task 3
 """  Написати функцію, яка розрахує середнє арифметичне списку чисел.
 """
+def avg_num(*data):
+    for num in data:
+        return sum(num) / len(num)
+avg_num([2,2,2,2])
 
 # task 4
 """  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
 """
+def reverse(text):
+    return text[::-1]
+reverse('Would you tell me, please, which way I ought to go from here?')
 
 # task 5
 """  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
 """
+def longest_word(word_list):
+    word_len = []
+    for x in word_list:
+        word_len.append((len(x), x))
+    word_len.sort()
+    return word_len[-1][1]
+
+longest_word(['apple', 'kiwi', 'cherry'])
+
 
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
 у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
 не є підрядком першого рядка."""
+
 def find_substring(str1, str2):
-
-    return -1
-
+    return str1.find(str2)
+ 
 str1 = "Hello, world!"
 str2 = "world"
 print(find_substring(str1, str2)) # поверне 7
@@ -69,3 +87,43 @@ print(find_substring(str1, str2)) # поверне -1
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
 Обоязково документуйте функції та дайте зрозумілі імена змінним.
 """
+
+def upper_count(text:str)-> int:
+    """ Function counts how many words in the text begin with a capital letter"""
+    return sum(word[0].isupper() for word in text)
+upper_count("Couldn't be much more from the heart\n"
+             "Forever trusting who we are\n"
+              "And nothing else matters")
+
+
+def characters_set(line:str)-> set:
+    """Set of all characters included in the given string"""
+    return set(list(line))
+
+characters_set("Something's wrong, shut the light, heavy thoughts tonight")
+
+def color_game(*colors:list)-> str:
+    """Awarding points depending on the selected color"""
+    for color in colors:
+        if color == 'green':
+            print('You just earned 5 points')
+        elif color == 'red':
+            print('You just earned 15 points')
+        else:
+            print('You just earned 10 points')
+
+color_game('green', 'yellow', 'red')
+
+def digits_sum(number:int) -> int:
+    """Calculate the sum of all digits of a natural number"""
+    sum = 0
+    while(number > 0):
+        dig = number % 10
+        sum = sum + dig
+        number = number//10
+    return sum
+
+digits_sum(12345)
+
+
+    
--- lession_08/test_homeworks08.py
+++ lession_08/test_homeworks08.py
@@ -3,3 +3,71 @@
 На оцінку впливає як якість тестів так і розмір тестового покриття.
 Мінімум на 10 балів - 1 правильно задизайнений позитивний тест на функцію.
 """
+
+import unittest
+import sys
+import pathlib
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+from lession_06 import homeworks06
+
+
+class HomeworksTesting(unittest.TestCase):
+
+    def test_task01(self):
+        actual_result = homeworks06.sum_num(0,1000)
+        expected_result = 1000
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+
+    def test_task02(self):
+        actual_result = homeworks06.sum_num(-1,-1)
+        expected_result = -2
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+    
+    def test_task03(self):
+        actual_result = homeworks06.sum_num(-3,7)
+        expected_result = 4
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+    
+    def test_task04(self):
+        str = ['apple', 'kiwi', 'cherry']
+        actual_result = homeworks06.longest_word(str)
+        expected_result = str[2]
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+
+    def test_task05(self):
+        str = ['a', 'b', 'c']
+        actual_result = homeworks06.longest_word(str)
+        expected_result = str[2]
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+    
+    def test_task06(self):
+        str = ['a']
+        actual_result = homeworks06.longest_word(str)
+        expected_result = str[0]
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+
+    def test_task07(self):
+        actual_result = homeworks06.digits_sum(12000345)
+        expected_result = 15
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")
+
+    def test_task08(self):
+        actual_result = homeworks06.digits_sum(0)
+        expected_result = 0
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")    
+
+    def test_task09(self):
+        actual_result = homeworks06.digits_sum(2147483647)
+        expected_result = 46
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")   
+
+    def test_task10(self):
+        actual_result = homeworks06.digits_sum(1)
+        expected_result = 1
+        self.assertEqual(actual_result, expected_result, msg=f"Actual result <{actual_result}> doesn't equal expected result <{expected_result}>")  
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
