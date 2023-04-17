e7c06b26f2be8a809fbed4170ec0e06e411b5c6a
Mykola Lahernyi <lagernuy88@gmail.com>
2023-04-17 16:44:38 +0300

    Add homework 8

--- lession_06/homeworks06.py
+++ lession_06/homeworks06.py
@@ -14,7 +14,7 @@ def multiplication_table(number):
         if  result > 25:
             # Enter the action to take if the result is greater than 25
             break
-        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+        #print(str(number) + "x" + str(multiplier) + "=" + str(result))
 
         # Increment the appropriate variable
         multiplier += 1
@@ -33,7 +33,7 @@ multiplication_table(3)
 """
 def sum1(a,b):
     return a+b
-print(sum1(5, 8))
+#print(sum1(5, 8))
 
 # task 3
 """  Написати функцію, яка розрахує середнє арифметичне списку чисел.
@@ -42,9 +42,7 @@ def arithmetic_average(number):
     suma = sum(number)
     average = suma/len(number)
     return average
-number = [2,4,6,8]
-result = arithmetic_average(number)
-print('середнє арифметичне: ', result)
+#print('середнє арифметичне: ', arithmetic_average([2,4,6,8]) )
 
 # task 4
 """  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
@@ -54,7 +52,7 @@ def reverse_order(ryadok):
     return reverse_ryadok
 ryadok = "Ой у лузі червона калина"
 result = reverse_order(ryadok)
-print("Зворотній рядок виглядає так:", result)
+#print("Зворотній рядок виглядає так:", result)
 
 # task 5
 """  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
@@ -67,7 +65,7 @@ def the_longest_word(word_list):
     return longest  
 word_list = ["яблуко", "груша", "апельсин", "банан", "виноград"]  
 result = the_longest_word(word_list)  
-print("Найдовше слово:", result)
+#print("Найдовше слово:", result)
 
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
@@ -79,11 +77,11 @@ def find_substring(str1, str2):
 
 str1 = "Hello, world!"
 str2 = "world"
-print(find_substring(str1, str2)) # поверне 7
+#print(find_substring(str1, str2)) # поверне 7
 
 str1 = "The quick brown fox jumps over the lazy dog"
 str2 = "cat"
-print(find_substring(str1, str2)) # поверне -1
+#print(find_substring(str1, str2)) # поверне -1
 
 """  Оберіть будь-які 4 таски з попередніх домашніх робіт та
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
@@ -97,7 +95,7 @@ def seas_square(black_sea, azov_sea): # створюємо функцію seas_s
     return black_sea + azov_sea       # вказуємо що будемо повертати
 black_sea = 436_402                   # задаємо перше значення
 azov_sea = 37_800                     # задаємо друге значення
-print(f"Загальна площа українських морів складає :", seas_square(black_sea, azov_sea)) #виводимо
+#print(f"Загальна площа українських морів складає :", seas_square(black_sea, azov_sea)) #виводимо
 # task 8
 """
 Іринка, готуючись до свого дня народження, склала список того,
@@ -113,7 +111,7 @@ print(f"Загальна площа українських морів склад
 def spusok_pokupok(*args):           # створюємо функцію , при цьому уявімо що ми не знаємо скільки і чого потрібно Ірині
     vsya_summa = sum(args)           # вказуєму що рахуємо суму елементів
     return(vsya_summa)               # виводимо  всю суму
-print("Загальна вартість покупок складає: ", spusok_pokupok((274*4),(218*2),(35*4),350,(21*3))) #виводимо результат нашої функції
+#print("Загальна вартість покупок складає: ", spusok_pokupok((274*4),(218*2),(35*4),350,(21*3))) #виводимо результат нашої функції
 # task 9
 """
 Родина зібралася в автомобільну подорож із Харкова в Буда-
@@ -130,7 +128,7 @@ def podorozh(distance, benz_na_sto, bak, sto=100):
     benz = (distance/sto)*benz_na_sto # рахуємо кількість бензину для подорожі
     zapravka = benz/bak # рахуємо скільки разів потрібно заправитись
     return f"Нам потрібно {benz} літрів бензину, потрібно завправитись {zapravka} рази" #функція повертатиме таку f-string
-print(podorozh(1600,9, 48)) # виводимо функцію зі значеннями із задачі
+#print(podorozh(1600,9, 48)) # виводимо функцію зі значеннями із задачі
 
 # task 10
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
@@ -158,7 +156,7 @@ rolling in wealth."""
 def kilkist_h():                # створюємо функцію
     kilkist = adwentures_of_tom_sawer.count('h')   # пишемо що саме вона буде робити 
     return f"В нашому тексті буква h зустрічається {kilkist} разів"    # прописуємо що саме вона буде повертати
-print(kilkist_h())              # друкуємо
+#print(kilkist_h())              # друкуємо
 
 
 
--- lession_08/homeworks08.py
+++ lession_08/homeworks08.py
@@ -2,4 +2,73 @@ import unittest
 import sys 
 import pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
-from lession_06 import homeworks06
+from lession_06.homeworks06 import*
+
+class HWTestings (unittest.TestCase):
+    def test_task01 (self):
+        """the sum of two positive numbers"""
+        actual_result = sum1(10, 5)
+        expected_result = 15
+        self.assertEqual(actual_result, expected_result, f"Sum of 10 and 5 is {sum1(10,5)}")
+    
+    def test_task02 (self):
+        """the sum of two negative numbers"""
+        actual_result = sum1(-10, -5)
+        expected_result = -15
+        self.assertEqual(actual_result, expected_result, "Sum of -10 and -5 is -15")
+    
+    def test_task03 (self):
+        """the sum of positive and negative numbers """
+        actual_result = sum1(-10,10)
+        expected_result = 0
+        self.assertEqual(actual_result, expected_result, "Sum of -10 and 10 is 0")
+    
+    def test_task04 (self):
+        """the sum of one number and 0"""
+        actual_result = sum1(10,0)
+        expected_result = 10
+        self.assertEqual(actual_result, expected_result, "Sum of 10 and 0 is 10")
+
+    def test_task05 (self):
+        """the sum of two zeros"""
+        actual_result = sum1(0,0)
+        expected_result = 0
+        self.assertTrue(actual_result==expected_result, "Sum of two zeros is zero")
+
+    def test_task06 (self):
+        """sum of two float numbers"""
+        actual_result = sum1(0.25, 0.75)
+        expected_result = 1
+        self.assertTrue(actual_result==expected_result, "Sum of 0.25 and 0.75 is 1")
+    def test_task07 (self):
+        """Arithmetic average of positive numbers"""
+        actual_result = arithmetic_average([2,4,6,8])
+        expected_result = 5
+        self.assertEqual(actual_result, expected_result, "Average of 2,4,6,8 is 5")
+
+    def test_task08 (self):
+        """Arithmetic average of negative numbers"""
+        actual_result = arithmetic_average([-2,-4,-6,-8])
+        expected_result = -5
+        self.assertEqual(actual_result,expected_result, "Average of -2,-4,-6,-8 is -5")
+    
+    def test_task09 (self):
+        """Arithmetic average of positive and negative numbers"""
+        actual_result = arithmetic_average([-5,5])
+        expected_result = 0
+        self.assertEqual(actual_result,expected_result, "Average of -5 and 5 is 0")
+    
+    def test_task10 (self):
+        """Arithmetic average of zeros"""
+        actual_result = arithmetic_average([0,0,0,0])
+        expected_result = 0
+        self.assertEqual(actual_result,expected_result, "Average of zeros is zero")
+    
+    def test_task11 (self):
+        """Arithmetic average of float numbers"""
+        actual_result = arithmetic_average([0.25, 0.75])
+        expected_result = 0.5
+        self.assertEqual(actual_result, expected_result, "Average of 0.25 and 0.75 is 0.5")
+        
+if __name__=="__main__":
+    unittest.main(verbosity = 2)
