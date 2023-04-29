95094b4ce8ad7834e7adda4e8851601b6451695c
Kliminchenko1 <Agevorobeyy1020@gmail.com>
2023-04-29 14:26:29 +0300

    fix

--- Home_work_10.py
+++ Home_work_10.py
@@ -1,34 +1,47 @@
 import json
 import random
 
+# Constants and enums
+
+
+class Action:
+    DIALOGUE = 1
+    FIGHT = 2
+    ESCAPE = 3
+
+
+CHARACTER_FILE = "characters.json"
+DESCRIPTION_CHOOSE_HERO = "Choose your hero:"
+DESCRIPTION_ENCOUNTER_ENEMY = "Your hero encountered {enemy}. What will you do?"
+DESCRIPTION_GAME_OVER = "The game is over. Your result: {result}."
 
 # Created a dictionary with character characteristics
-characters = {
+CHARACTERS = {
     "Catigoroshko": {"health": 100, "strength": 10},
     "Snake": {"health": 80, "strength": 15},
     "Horse": {"health": 120, "strength": 8}
 }
 
 # Save the dictionary of characters to a file
-with open("characters.json", "w") as f:
-    json.dump(characters, f)
+with open(CHARACTER_FILE, "w") as f:
+    json.dump(CHARACTERS, f)
 
 # Load the dictionary of characters from a file
-with open("characters.json", "r") as f:
-    characters = json.load(f)
+with open(CHARACTER_FILE, "r") as f:
+    CHARACTERS = json.load(f)
 
 # Define the stages of the game
-stages = [
+STAGES = [
     {
-        "description": "Choose your hero:",
-        "actions": characters.keys()
+        "description": DESCRIPTION_CHOOSE_HERO,
+        "actions": CHARACTERS.keys()
     },
     {
-        "description": "Your hero encountered {enemy}. What will you do?",
-        "actions": ["Dialogue", "Fight", "Escape"]
+        "description": DESCRIPTION_ENCOUNTER_ENEMY,
+        "actions": [Action.DIALOGUE, Action.FIGHT, Action.ESCAPE]
     },
     {
-        "description": "The game is over. Your result: {result}."
+        "description": DESCRIPTION_GAME_OVER
     }
 ]
 
@@ -37,11 +50,11 @@ stages = [
 
 def select_character():
     print("Available characters:")
-    for name, stats in characters.items():
+    for name, stats in CHARACTERS.items():
         print(
             f"{name} (health: {stats['health']}, strength: {stats['strength']})")
     character = input("Choose your hero: ")
-    if character not in characters:
+    if character not in CHARACTERS:
         raise ValueError("Invalid character selection")
     return character
 
@@ -49,10 +62,9 @@ def select_character():
 def encounter(enemy):
     print(f"Your hero met {enemy}. What will you do?")
     while True:
-        action = input("1. Dialogue 2. Fight 3. Escape\n")
         try:
-            action = int(action)
-            if action not in [1, 2, 3]:
+            action = int(input("1. Dialogue 2. Fight 3. Escape\n"))
+            if action not in [Action.DIALOGUE, Action.FIGHT, Action.ESCAPE]:
                 raise ValueError(
                     "Invalid action selection. Please choose 1, 2, or 3.")
             break
@@ -62,8 +74,8 @@ def encounter(enemy):
 
 
 def battle(player, enemy):
-    player_stats = characters[player]
-    enemy_stats = characters[enemy]
+    player_stats = CHARACTERS[player]
+    enemy_stats = CHARACTERS[enemy]
     print(
         f"{player} (health: {player_stats['health']}, strength: {player_stats['strength']}) vs {enemy} (health: {enemy_stats['health']}, strength: {enemy_stats['strength']})")
     player_damage = random.randint(1, player_stats['strength'])
@@ -79,44 +91,51 @@ def battle(player, enemy):
     else:
         return None
 
-# Main game function
+# Main Game
 
 
 def play_game():
-    # Choosing a character
-    try:
-        character = select_character()
-    except ValueError:
-        print("Invalid character selection. Please choose one of the available characters.")
-        return
-
-    # Main game loop
+    stage_index = 0
+    player = None
+    enemy = None
     result = None
-    for stage in stages:
-        if stage["description"] == "Your hero encountered {enemy}. What will you do?":
-            enemy = random.choice(
-                [c for c in characters.keys() if c != character])
-            stage["description"] = stage["description"].format(enemy=enemy)
+
+    while stage_index < len(STAGES):
+        stage = STAGES[stage_index]
+
+        # Handle the choose hero stage
+        if stage_index == 0:
+            player = select_character()
+            print(f"You have chosen {player} as your hero.")
+
+        # Handle the encounter enemy stage
+        elif stage_index == 1:
+            enemy = random.choice(list(CHARACTERS.keys()))
+            print(stage['description'].format(enemy=enemy))
             action = encounter(enemy)
-            if action == 1:
-                print(f"{character} attempts to engage {enemy} in dialogue.")
-            elif action == 2:
-                result = battle(character, enemy)
-                if result == "tie":
-                    print("Both heroes struck each other down. It's a tie.")
-                    break
-                elif result == "victory":
-                    print(f"{character} defeated {enemy} in battle!")
+            if action == Action.DIALOGUE:
+                print("You tried to talk to the enemy, but they didn't seem interested.")
+            elif action == Action.FIGHT:
+                result = battle(player, enemy)
+                if result == "victory":
+                    print(f"You defeated {enemy}! Congratulations!")
+                elif result == "defeat":
+                    print(f"You were defeated by {enemy}. Game over.")
                 else:
-                    print(f"{character} was defeated by {enemy} in battle.")
-                    break
+                    print("It's a tie! Both you and the enemy have been defeated.")
+            elif action == Action.ESCAPE:
+                print("You run away from the enemy. Coward.")
+
+        # Handle the game over stage
+        elif stage_index == 2:
+            if result == "victory":
+                print(stage['description'].format(result="victory!"))
             else:
-                print(f"{character} fled from {enemy}.")
-                break
-        elif stage["description"] == "The game has ended. Your result: {result}.":
-            stage["description"] = stage["description"].format(result=result)
-            print(stage["description"])
-            break
+                print(stage['description'].format(result="defeat."))
+            return
+
+        # Move on to the next stage
+        stage_index += 1
 
 
 play_game()
