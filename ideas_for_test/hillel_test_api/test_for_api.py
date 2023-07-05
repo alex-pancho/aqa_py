b9ba9d724e71cef70c7d4e5d026cb92188ceb2ad
arseniivetrov <arsvetrov@gmail.com>
2023-07-05 23:25:01 +0300

    hw_fixes+ refactoring

--- ideas_for_test/hillel_test_api/test_for_api.py
+++ ideas_for_test/hillel_test_api/test_for_api.py
@@ -3,28 +3,44 @@ import pytest
 
 
 @pytest.fixture()
-def new_user():
-
+def registered_user():
     # create user
     register_data = {
         "name": "Anatolii",
         "lastName": "Pepsi",
-        "email": "apepsi@test.com",
+        "email": "apepsii@test.com",
         "password": "Qam2608venv#",
         "repeatPassword": "Qam2608venv#"
-        }
+    }
+    r = auth.signup(s, register_data)
+    yield r
+
+    # delete user
+    users.users(s)
+
+
+@pytest.fixture()
+def logged_user():
+    # create user
+    register_data = {
+        "name": "Anatolii",
+        "lastName": "Pepsi",
+        "email": "apepsii@test.com",
+        "password": "Qam2608venv#",
+        "repeatPassword": "Qam2608venv#"
+    }
     auth.signup(s, register_data)
 
     # login user
     login_data = {
-        "email": "apepsi@test.com",
+        "email": "apepsii@test.com",
         "password": "Qam2608venv#",
         "remember": True
-        }
-    r = auth.signin(s, login_data)
-    yield r
+    }
 
-    # delete test user
+    yield auth.signin(s, login_data)
+
+    # delete user
     users.users(s)
 
 
@@ -34,7 +50,7 @@ def test_signup_positive():
     user_data = {
         "name": "Anatolii",
         "lastName": "Pepsi",
-        "email": "apepsi@test.com",
+        "email": "apepsii2@test.com",
         "password": "Qam2608venv#",
         "repeatPassword": "Qam2608venv#"
     }
@@ -49,13 +65,13 @@ def test_signup_positive():
     assert r_json["status"] == 'ok', "Key 'status' is not 'ok'"
 
 
-def test_signup_negative():
+def test_signup_negative(registered_user):
     """Try Register existing user """
 
     user_data = {
         "name": "Anatolii",
         "lastName": "Pepsi",
-        "email": "pepsi@test.com",
+        "email": "apepsii@test.com",
         "password": "Qam2608venv#",
         "repeatPassword": "Qam2608venv#"
     }
@@ -70,14 +86,14 @@ def test_signup_negative():
     users.users(s)
     assert r.status_code == 400, "Wrong status code"
     assert r_json["status"] == 'error', "Key 'status' is not 'error'"
-    assert r_json["message"] == 'User already exists'
+    assert r_json["message"] == 'User already exists', 'Incorrect message received'
 
 
-def test_sigin_positive():
+def test_signin_positive(registered_user):
     """Login existing user"""
 
     user_data = {
-        "email": "qam2608@2023test.com",
+        "email": "apepsii@test.com",
         "password": "Qam2608venv#",
         "remember": False
     }
@@ -87,30 +103,31 @@ def test_sigin_positive():
     assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
 
 
-def test_sigin_negative():
+def test_signin_negative(registered_user):
     """Login incorrect password"""
 
     user_data_negative = {
-        "email": "qam@2023test.com",
-        "password": "Qam2",
+        "email": "apepsii@test.com",
+        "password": "testam2608venv#",
         "remember": False
     }
     r = auth.signin(s, user_data_negative)
     r_json = after_processsing(r)
     assert r.status_code == 400, "Wrong status code"
     assert r_json["status"] == "error", "Key 'status' is not 'error'"
+    assert r_json["message"] == 'Wrong email or password', 'Incorrect message received'
 
 
-def test_logout_positive():
+def test_logout_positive(logged_user):
     """Logout user"""
 
     r = auth.logout(s)
-    r_json = after_processsing(r)    
+    r_json = after_processsing(r)
     assert r.status_code == 200, "Wrong status code"
     assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
 
 
-def test_reset_password_positive(new_user):
+def test_reset_password_positive(logged_user):
     """Reset pass"""
 
     user_data = {
@@ -126,8 +143,8 @@ def test_reset_password_positive(new_user):
     assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
 
 
-def test_reset_password_negative(new_user):
-    """Reset pass passwords doesn't match"""
+def test_reset_password_negative(logged_user):
+    """Reset pass old a new passwords don't match"""
 
     user_data = {
         "oldPassword": "Qam2608venv",
@@ -143,29 +160,49 @@ def test_reset_password_negative(new_user):
     assert r_json["message"] == "Wrong password", "Incorrect error message"
 
 
-def test_change_email_positive(new_user):
+def test_reset_password_negative2(logged_user):
+    """Reset pass new passwords don't match"""
+
+    user_data = {
+        "oldPassword": "Qam2608venv#",
+        "password": "123Abc+-=",
+        "repeatPassword": "123Abc+-"
+    }
+
+    r = users.resetpassword(s, user_data)
+    r_json = after_processsing(r)
+
+    assert r.status_code == 400, "Wrong status code"
+    assert r_json["status"] == "error", "Key 'status' is not 'ok'"
+    assert r_json["message"] == "Passwords do not match", "Incorrect error message"
+
+
+def test_change_email_positive(logged_user):
     """Change users email"""
 
     user_data = {
-        "email": "test1323123@test.com",
+        "email": "testanatolii@gmail.com",
         "password": "Qam2608venv#"
     }
 
     r = users.email(s, user_data)
     r_json = after_processsing(r)
 
-    assert r.status_code == 200, "Wrong status code"
+    assert r.status_code == 200, f"Wrong status code"
     assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
 
 
-def test_current_positive(new_user):
+def test_current_positive(logged_user):
     """Get user info"""
 
     r = users.current(s)
     r_json = after_processsing(r)
 
+    # Get test user json
+    r_users_json = after_processsing(logged_user)
+
     assert r.status_code == 200, "Wrong status code"
-    assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
+    assert r_json == r_users_json, "Current user's data is not correct"
 
 
 def test_current_negative():
@@ -179,7 +216,7 @@ def test_current_negative():
     assert r_json["message"] == "Not authenticated", "Incorrect error message"
 
 
-def test_profile_put(new_user):
+def test_profile_put_positive(logged_user):
     """Update user data"""
     new_user_data = {
 
@@ -197,34 +234,27 @@ def test_profile_put(new_user):
     assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
 
 
-def test_settings_get(new_user):
+def test_settings_get_positive(logged_user):
     """Get user settings"""
 
     r = users.settings_get(s)
     r_json = after_processsing(r)
     expected_json = {
-                      "status": "ok",
-                      "data": {
-                          "currency": "usd",
-                          "distanceUnits": "km"
-                        }
+        "status": "ok",
+        "data": {
+            "currency": "usd",
+            "distanceUnits": "km"
+        }
     }
 
     assert r.status_code == 200, "Wrong status code"
     assert r_json == expected_json, "Wrong json response received"
 
 
-def test_delete_user(new_user):
+def test_delete_user_positive(logged_user):
     """Delete user"""
     r = users.users(s)
     r_json = after_processsing(r)
 
-    assert r.status_code == 200
-    assert r_json["status"] == "ok"
-
-
-
-
-
-
-
+    assert r.status_code == 200, "Wrong status code"
+    assert r_json["status"] == "ok", "Key 'status' is not 'ok'"
