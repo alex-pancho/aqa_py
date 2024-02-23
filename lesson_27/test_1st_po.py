8bfa86de4cc79fb0d165d6e828d3e3afee965c0a
OPanchenko <pan.pan.4it@gmail.com>
2024-02-23 10:16:55 +0200

    fix

--- lesson_27/test_1st_po.py
+++ lesson_27/test_1st_po.py
@@ -2,15 +2,14 @@ from get_browser import firefox
 from first_page_object import SignInPage
 
 
-URL = "https://semantic-ui.com/examples/login.html"
+LOGIN_URL = "https://semantic-ui.com/examples/login.html"
 
 
 def test_login():
     driver = firefox()
-    sign_in = SignInPage(driver, URL)
+    sign_in = SignInPage(driver, LOGIN_URL)
     assert sign_in.driver is not None
     user = "user@gmail.com"
     password = "my supersecret password"
-
     afrer_login = sign_in.login_valid_user(user, password)
     assert afrer_login.get_message_text() == "Log-in to your account"
